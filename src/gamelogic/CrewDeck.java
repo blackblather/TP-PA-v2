43f077e0abaec4fed97268b26cabc531eeacb630
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-06 20:48:28 +0100

    Created class. Maior parte dos metodos estao feitos. Falta adicionar as cartas

---
+++ src/gamelogic/CrewDeck.java
@@ -0,0 +1,19 @@
+package gamelogic;
+
+import java.util.ArrayList;
+
+public class CrewDeck {
+    private ArrayList<CrewMember> cards = new ArrayList<>(12);
+    public CrewDeck(){
+        //cards.add(new CrewMember("Doctor",1, 106, 3));
+    }
+    public ArrayList<CrewMember> GetCards(){
+        return cards;
+    }
+    public CrewMember ChooseCardAt(Integer pos){
+        CrewMember crewMember = cards.get(pos);
+        if(crewMember.Choose())
+            return crewMember;
+        return null;
+    }
+}
