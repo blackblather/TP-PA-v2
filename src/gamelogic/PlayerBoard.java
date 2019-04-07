66bee3bce5a6a7b20eacf403a658857c7e081b0f
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-07 23:50:11 +0100

    Added private vars, constructor and the AddCrewMember() functions

--- src/gamelogic/PlayerBoard.java
+++ src/gamelogic/PlayerBoard.java
@@ -1,4 +1,26 @@
 package gamelogic;
 
-public class playerBoard {
+import java.util.ArrayList;
+
+public class PlayerBoard {
+    //Private vars
+    private ArrayList<CrewMember> crewMembers = new ArrayList<>();
+    private Integer inspirationTracker = 0, healthTracker, actionPointsPerTurn, actionPoints;
+    //Constructor
+    public PlayerBoard(){
+        healthTracker = 8;
+    }
+    boolean AddCrewMember(int pos){
+        CrewDeck deck = new CrewDeck();
+        try {
+            CrewMember crewMember = deck.ChooseCardAt(pos);
+            if(crewMember != null){
+                crewMembers.add(crewMember);
+                return true;
+            }
+        }catch (IndexOutOfBoundsException ex){
+            return false;
+        }
+        return false;
+    }
 }
