58ba2dda909c0da8c0a527326012b82b2efb773f
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-07 23:42:04 +0100

    Added private vars, getters, package-private functions, realized the attacks são os numeros de dados D6 (it's 1D6, not 106), also added minimumAttackRoll

--- src/gamelogic/CrewMember.java
+++ src/gamelogic/CrewMember.java
@@ -1,19 +1,68 @@
 package gamelogic;
 
-public class crewMember {
-    private Integer movement, attack;
+import java.util.concurrent.ThreadLocalRandom;
 
+public class CrewMember {
+    //Private vars
+    private String name;
+    private Integer movement, attack, minimumAttackRoll = 5;
+    private boolean isAvailable = true;
+    //-------------------------------------------------
+    //Private functions
     private boolean IsValidMovementValue(Integer movement){
-        return (movement >= 1 && movement <= 3);
+        //0 means teleport
+        return (movement >= 0 && movement <= 3);
     }
     private boolean IsValidAttackValue(Integer attack){
-        return (attack == 106 || attack == 206 || attack == 306);
+        return (attack >= 1 && attack <= 3);
     }
-
-    public crewMember(Integer movement, Integer attack){
+    private boolean IsAvailable(){
+        return isAvailable;
+    }
+    //-------------------------------------------------
+    //Constrcutor
+    public CrewMember(String name, Integer movement, Integer attack){
         if(IsValidMovementValue(movement) && IsValidAttackValue(attack)){
+            this.name = name;
             this.movement = movement;
             this.attack = attack;
         }
     }
+    public CrewMember(String name, Integer movement, Integer attack, Integer minimumAttackRoll){
+        if(IsValidMovementValue(movement) && IsValidAttackValue(attack)){
+            this.name = name;
+            this.movement = movement;
+            this.attack = attack;
+            this.minimumAttackRoll = minimumAttackRoll;
+        }
+    }
+    //-------------------------------------------------
+    //Package-private functions
+    boolean Choose(){
+        if(IsAvailable()){
+            isAvailable = false;
+            return true;
+        }
+        return false;
+    }
+    Integer RollAttackDice(){
+        //rangre = [attack, (attack*6)+1]
+        return ThreadLocalRandom.current().nextInt(attack, (attack*6)+1);
+    }
+    //-------------------------------------------------
+    //Getters
+    public String GetName(){
+        return name;
+    }
+    public Integer GetMovement(){
+        return movement;
+    }
+    public Integer GetAttack(){
+        return attack;
+    }
+    public Integer GetMinimumAttackRoll(){
+        return minimumAttackRoll;
+    }
+    //-------------------------------------------------
+
 }
