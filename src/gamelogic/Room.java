dd000ac648339d62d7ca68e30883b1d0c4196f2a
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-07 23:15:24 +0100

    Added Constructor, Getters, IsSealed() and SealRoom()

--- src/gamelogic/Room.java
+++ src/gamelogic/Room.java
@@ -1,7 +1,32 @@
 package gamelogic;
 
+import javax.crypto.IllegalBlockSizeException;
+
 public class Room {
+    //Private vars
     private Integer id;
     private String name;
     private boolean sealed = false;
+    //Constructor
+    Room(Integer id, String name){
+        this.id = id;
+        this.name = name;
+    }
+    //Package-Protected functions
+    boolean IsSealed(){
+        return sealed;
+    }
+    void SealRoom() throws IllegalStateException {
+        if(!IsSealed())
+            sealed = true;
+        else
+            throw new IllegalStateException("The rrCant' seal a room that's already been sealed.");
+    }
+    //Getters
+    Integer GetId(){
+        return id;
+    }
+    String GetName(){
+        return name;
+    }
 }
