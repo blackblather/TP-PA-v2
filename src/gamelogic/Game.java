e565c62901905e2685d0e16f778e474bdc989f61
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-08 10:19:36 +0100

    Criei um código muito rudimentar da máquina de estados.

--- src/gamelogic/Game.java
+++ src/gamelogic/Game.java
@@ -3,5 +3,14 @@ package gamelogic;
 public class Game {
     ShipBoard shipBoard;
     PlayerBoard playerBoard;
-
+    GameSetupState gameSetupState;
+    GameState gameState;
+    public void GameSetupStateMachine(){
+        switch (gameSetupState){
+        }
+    }
+    public void GameStateMachine(){
+        switch (gameState){
+        }
+    }
 }
