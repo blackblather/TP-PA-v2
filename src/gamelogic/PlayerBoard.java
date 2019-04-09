0fc0b6a533a189b48abd6d003c058e5d3b5a68f9
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-09 23:24:23 +0100

    Added "alienCounter" and "DecreaseAlienCounterBy()"

--- src/gamelogic/PlayerBoard.java
+++ src/gamelogic/PlayerBoard.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 public class PlayerBoard {
     //Private vars
     private ArrayList<CrewMember> crewMembers = new ArrayList<>();
-    private Integer inspirationTracker = 0, healthTracker, actionPointsPerTurn, actionPoints;
+    private Integer inspirationTracker = 0, healthTracker, actionPointsPerTurn, actionPoints, alienCounter = 15;
     //Constructor
     public PlayerBoard(){
         healthTracker = 8;
@@ -22,5 +22,14 @@ public class PlayerBoard {
             return false;
         }
         return false;
+    }
+    int DecreaseAlienCounterBy(int ammount){
+        if(alienCounter-ammount >= 0)
+            alienCounter -= ammount;
+        else{
+            ammount = alienCounter;
+            alienCounter = 0;
+        }
+        return ammount;
     }
 }
