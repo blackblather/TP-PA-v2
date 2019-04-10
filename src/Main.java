8d45c91d750a0604c49774471fc9123786048037
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-10 17:19:41 +0100

    Moved Main file out of ui.text package

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import gamelogic.Game;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*Scanner scanner = new Scanner(System.in);
+        int option;
+        do{
+            System.out.print("Initial Menu:\n (1) - New Game\n (2) - Exit\nOption: ");
+            option = scanner.nextInt();
+        } while(option != 2);*/
+
+        /*Game DestinationEarth = new Game();
+        DestinationEarth.GameSetupStateMachine();*/
+        String x = "y";
+        String[] parts = x.split("y");
+
+        System.out.println(parts[0]);
+        System.out.println(parts[1]);
+    }
+}
--- src/ui/text/Main.java
+++
@@ -1,8 +0,0 @@
-package ui.text;
-import gamelogic.*;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
