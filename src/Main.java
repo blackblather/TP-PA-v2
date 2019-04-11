1f6995f63eff04dea349ea749c70ec694724a8af
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-11 20:56:56 +0100

    main function only calls TextUI.Start() now

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
-import gamelogic.Game;
-
-import java.util.Scanner;
+import ui.text.TextUI;
 
 public class Main {
     public static void main(String[] args) {
-        /*Scanner scanner = new Scanner(System.in);
-        int option;
-        do{
-            System.out.print("Initial Menu:\n (1) - New Game\n (2) - Exit\nOption: ");
-            option = scanner.nextInt();
-        } while(option != 2);*/
-
-        /*Game DestinationEarth = new Game();
-        DestinationEarth.GameSetupStateMachine();*/
-        String x = "y";
-        String[] parts = x.split("y");
-
-        System.out.println(parts[0]);
-        System.out.println(parts[1]);
+        TextUI textUI = new TextUI();
+        textUI.Start();
     }
 }
