8712877677b376943b6c177ea4835eede96c9822
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-12 21:45:06 +0100

    maxActionPoints has a default value of 5 and "AddCrewMember()" doesn't catch exceptions anymore (now that's done by the state machine)

--- src/gamelogic/data/PlayerBoard.java
+++ src/gamelogic/data/PlayerBoard.java
@@ -5,22 +5,14 @@ import java.util.ArrayList;
 class PlayerBoard {
     //Private vars
     private ArrayList<CrewMember> crewMembers = new ArrayList<>();
-    private Integer inspirationPoints = 0, health = 8, maxActionPoints, actionPoints, alienCounter = 15;
+    private Integer inspirationPoints = 0, health = 8, maxActionPoints = 5, actionPoints, alienCounter = 15;
     ArrayList<CrewMember> GetCrewMembers(){
         return crewMembers;
     }
-    boolean AddCrewMember(int pos){
-        Deck deck = new Deck();
-        try {
-            CrewMember crewMember = deck.ChooseCardAt(pos);
-            if(crewMember != null){
-                crewMembers.add(crewMember);
-                return true;
-            }
-        }catch (IndexOutOfBoundsException ex){
-            return false;
-        }
-        return false;
+    void AddCrewMember(Deck deck, int pos) throws IndexOutOfBoundsException, IllegalAccessException {
+        CrewMember crewMember = deck.ChooseCardAt(pos);
+        if(crewMember != null)
+            crewMembers.add(crewMember);
     }
     int DecreaseAlienCounterBy(int ammount){
         if(alienCounter-ammount >= 0)
