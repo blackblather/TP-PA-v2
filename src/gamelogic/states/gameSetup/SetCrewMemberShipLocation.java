68baf69a102509c7a15fb0fa9b9f22fe29787067
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-13 21:37:57 +0100

    Added params "roomPos" and "crewMemberPos" to "_SetCrewMemberShipLocation()", and created body for "_SetCrewMemberShipLocation()"

--- src/gamelogic/states/gameSetup/SetCrewMemberShipLocation.java
+++ src/gamelogic/states/gameSetup/SetCrewMemberShipLocation.java
@@ -3,12 +3,20 @@ package gamelogic.states.gameSetup;
 import gamelogic.data.EncapsulatedGameData;
 
 public class SetCrewMemberShipLocation extends GameSetupStateAdapter {
-    public SetCrewMemberShipLocation(EncapsulatedGameData encapsulatedGameData) {
+    SetCrewMemberShipLocation(EncapsulatedGameData encapsulatedGameData) {
         super(encapsulatedGameData);
     }
 
     @Override
-    public IGameSetupState _SetCrewMemberShipLocation() {
-        return new StartGame(encapsulatedGameData);
+    public IGameSetupState _SetCrewMemberShipLocation(int roomPos, int crewMemberPos) {
+        try{
+            encapsulatedGameData.MoveCrewMemberToRoom(roomPos, crewMemberPos);
+            if(encapsulatedGameData.GetTotalCrewMembersInRooms() < 2)
+                return this;
+            else
+                return new StartGame(encapsulatedGameData);
+        } catch (Exception ex){
+            return this;
+        }
     }
 }
