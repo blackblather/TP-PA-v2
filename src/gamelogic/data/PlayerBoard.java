574e74c18a6d4a6d1e00c0f74d104d180baff426
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-13 21:42:15 +0100

    Added "GetCrewMemberAt()" and "CrewMemberToStringAt" (to use in EncapsulatedGameData) Last one may be redundant

--- src/gamelogic/data/PlayerBoard.java
+++ src/gamelogic/data/PlayerBoard.java
@@ -6,9 +6,12 @@ class PlayerBoard {
     //Private vars
     private ArrayList<CrewMember> crewMembers = new ArrayList<>();
     private Integer inspirationPoints = 0, health = 8, maxActionPoints = 5, actionPoints, alienCounter = 15;
+
+    //Package-Private functions
     ArrayList<CrewMember> GetCrewMembers(){
         return crewMembers;
     }
+    CrewMember GetCrewMemberAt(int pos){ return crewMembers.get(pos); }
     void AddCrewMember(Deck deck, int pos) throws IndexOutOfBoundsException, IllegalAccessException {
         CrewMember crewMember = deck.ChooseCardAt(pos);
         if(crewMember != null)
@@ -22,5 +25,8 @@ class PlayerBoard {
             alienCounter = 0;
         }
         return ammount;
+    }
+    String CrewMemberToStringAt(int pos) throws IndexOutOfBoundsException{
+        return crewMembers.get(pos).toString();
     }
 }
