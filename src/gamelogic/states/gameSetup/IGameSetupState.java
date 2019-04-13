22a25807c2d3d64b98683c9d450e4ece94a62b57
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-13 21:37:26 +0100

    Added params "roomPos" and "crewMemberPos" to "_SetCrewMemberShipLocation()"

--- src/gamelogic/states/gameSetup/IGameSetupState.java
+++ src/gamelogic/states/gameSetup/IGameSetupState.java
@@ -5,7 +5,7 @@ public interface IGameSetupState {
     IGameSetupState _ChooseJourney();                           //Default Journey
     IGameSetupState _ChooseJourney(String[] customJourney);     //Custom Journey
     IGameSetupState _SelectCrewMembers(int pos);
-    IGameSetupState _SetCrewMemberShipLocation();
+    IGameSetupState _SetCrewMemberShipLocation(int roomPos, int crewMemberPos);
     IGameSetupState _StartGame();
     IGameSetupState _EndGame();
     IGameSetupState _Exit();
