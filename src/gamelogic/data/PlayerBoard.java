4e193807dcd222e5c15a22c679aa2c0c3c0925e9
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-17 01:12:00 +0100

    Added private var "organicDetonatorCounter", setters "DecrementOrganicDetonatorCounter", "IncrementOrganicDetonatorCounter" and "HealHealthBy" and getters "GetCrewMembers", "GetCrewMemberAt", "GetHealth" and "GetIspirationPoints",

--- src/gamelogic/data/PlayerBoard.java
+++ src/gamelogic/data/PlayerBoard.java
@@ -2,21 +2,41 @@ package gamelogic.data;
 
 import java.util.ArrayList;
 
-class PlayerBoard {
+class PlayerBoard extends Board {
     //Private vars
     private ArrayList<CrewMember> crewMembers = new ArrayList<>();
-    private Integer inspirationPoints = 0, health = 8, maxActionPoints = 5, actionPoints, alienCounter = 15;
+    private int inspirationPoints = 0,
+            health = 8,
+            maxActionPoints = 5,
+            actionPoints,
+            alienCounter = Constants.MAX_ALIENS,
+            organicDetonatorCounter = Constants.MAX_ORGANIC_DETONATORS;
 
     //Package-Private functions
-    ArrayList<CrewMember> GetCrewMembers(){
-        return crewMembers;
+    String CrewMemberToStringAt(int pos) throws IndexOutOfBoundsException{
+        return crewMembers.get(pos).toString();
     }
-    CrewMember GetCrewMemberAt(int pos){ return crewMembers.get(pos); }
     void AddCrewMember(Deck deck, int pos) throws IndexOutOfBoundsException, IllegalAccessException {
         CrewMember crewMember = deck.ChooseCardAt(pos);
         if(crewMember != null)
             crewMembers.add(crewMember);
     }
+
+    //Getters
+    ArrayList<CrewMember> GetCrewMembers(){
+        return crewMembers;
+    }
+    CrewMember GetCrewMemberAt(int pos){
+        return crewMembers.get(pos);
+    }
+    int GetHealth(){
+        return health;
+    }
+    int GetIspirationPoints(){
+        return inspirationPoints;
+    }
+
+    //Setters
     int DecreaseAlienCounterBy(int ammount){
         if(alienCounter-ammount >= 0)
             alienCounter -= ammount;
@@ -26,11 +46,19 @@ class PlayerBoard {
         }
         return ammount;
     }
-
-    String CrewMemberToStringAt(int pos) throws IndexOutOfBoundsException{
-        return crewMembers.get(pos).toString();
+    void HealHealthBy(int ammount){
+        if(health+ammount <= Constants.MAX_HEALTH)
+            health += ammount;
+        else{
+            health = Constants.MAX_HEALTH;
+        }
     }
-
-    //Getters
-    int GetHealth(){ return health; }
+    void DecrementOrganicDetonatorCounter(){
+        if(organicDetonatorCounter > 0)
+            organicDetonatorCounter--;
+    }
+    void IncrementOrganicDetonatorCounter(){
+        if(organicDetonatorCounter < Constants.MAX_ORGANIC_DETONATORS)
+            organicDetonatorCounter++;
+    }
 }
