57f6b660947093f853e344da354b09d0d199fcbc
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-17 01:18:04 +0100

    Added Constants class to aggregate symbolic constants

---
+++ src/gamelogic/data/Constants.java
@@ -0,0 +1,18 @@
+package gamelogic.data;
+
+public class Constants {
+    /*Helper sources:
+       -> Source #1: https://stackoverflow.com/questions/66066/what-is-the-best-way-to-implement-constants-in-java
+       -> Source #2: https://www.thoughtco.com/constant-2034049
+    */
+    private Constants(){}
+
+    public static final int MAX_HEALTH = 12;
+    public static final int MAX_JOURNEY_PARTS = 13;
+    public static final int MAX_HULL = 13;
+    public static final int MAX_INSPIRATION_POINTS = 12;
+    public static final int MAX_ORGANIC_DETONATORS = 4;
+    public static final int MAX_PARTICLE_DISPERSERS = 2;
+    public static final int MAX_ALIENS = 15;
+    public static final int MAX_SEALED_ROOMS = 6;
+}
