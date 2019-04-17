c9af062b98fdcca17f9aedda0302283e3d8851d9
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-17 01:16:48 +0100

    Added base class for effects (upgrades (rest phase) and actions (crew phase))

---
+++ src/gamelogic/data/Effect.java
@@ -0,0 +1,55 @@
+package gamelogic.data;
+
+import java.security.InvalidParameterException;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+class Effect{
+    //Private vars
+    private int cost;
+    private String description;
+    private Consumer<GameDataHandler> simpleEffect = null;
+    private BiConsumer<GameDataHandler, Integer> effectWithValue = null;
+
+    //Constructor
+    Effect(int cost, String description, Consumer<GameDataHandler> simpleEffect) throws InvalidParameterException {
+        if(simpleEffect == null)
+            throw new InvalidParameterException("Effect cannot be null.");
+        this.cost = cost;
+        this.description = description;
+        this.simpleEffect = simpleEffect;
+    }
+
+    Effect(int cost, String description, BiConsumer<GameDataHandler, Integer> effectWithValue) throws InvalidParameterException {
+        if(effectWithValue == null)
+            throw new InvalidParameterException("Effect cannot be null.");
+        this.cost = cost;
+        this.description = description;
+        this.effectWithValue = effectWithValue;
+    }
+
+    //Getters
+    int GetCost(){
+        return cost;
+    }
+    String GetDescription(){
+        return description;
+    }
+    boolean NeedsAditionalInput(){
+        return (effectWithValue != null);
+    }
+
+    //Package-protected functions
+    void ExecuteEffect(GameDataHandler gameDataHandler){
+        simpleEffect.accept(gameDataHandler);
+    }
+    void ExecuteEffect(GameDataHandler gameDataHandler, Integer value) throws IndexOutOfBoundsException{
+        effectWithValue.accept(gameDataHandler,value);
+    }
+
+    //Overrides -> Used mainly in the TextUI
+    @Override
+    public String toString(){
+        return ("Cost: " + cost + "\nDescription: " + description);
+    }
+}
