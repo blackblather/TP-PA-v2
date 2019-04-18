07ebcfa342431bfcfc49c3d6644b5ba1fc12ab7f
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-04-18 20:13:05 +0100

    Added (and tested) effect #4

--- src/gamelogic/data/Upgrades.java
+++ src/gamelogic/data/Upgrades.java
@@ -7,14 +7,17 @@ class Upgrades {
     //Private vars
     private GameDataHandler gameDataHandler;
     private ArrayList<Effect> upgrade = new ArrayList<>(Arrays.asList(
-            new Effect(1, "Add on to Health", (gdh)->gdh.GetPlayerBoard().HealHealthBy(1)),
+            new Effect(1, "Add one to Health", (gdh)->gdh.GetPlayerBoard().HealHealthBy(1)),
             new Effect(1, "Repair one Hull", (gdh)->gdh.GetShipBoard().HealHullBy(1)),
-            new Effect(2, "Room","Build one Organic Detonator", (gdh, val)->{
-                    Room room = gdh.GetShipBoard().GetRooms().get(val);
-                    gdh.GetPlayerBoard().DecrementOrganicDetonatorCounter();
-                    room.IncrementOrganicDetonatorCounter();
-            })
-            /*new Effect(4, "Add one to Movement"),
+            new Effect(2, "room", "Build one Organic Detonator", (gdh, val)->{
+                    Room room = gdh.GetShipBoard().GetRooms().get(val);         //Throws exception if it tries to get an invalid position
+                    gdh.GetPlayerBoard().DecrementOrganicDetonatorCounter();    //Throws exception when trying to decrement below 0
+                    room.IncrementOrganicDetonatorCounter();                    //Throws exception when trying to increment above Constants.MAX_ORGANIC_DETONATORS (never going to happen on this scenario)
+            }),
+            new Effect(4, "crew member", "Add one to Movement", (gdh, val)->{
+                CrewMember crewMember = gdh.GetPlayerBoard().GetCrewMembers().get(val);
+                crewMember.SetMovement(crewMember.GetMovement()+1);
+            })/*,
             new Effect(5, "Build one particle Desperser"),
             new Effect(5, "Gain one Sealed Room Token"),
             new Effect(6, "Gain one extra Attack Die"),
