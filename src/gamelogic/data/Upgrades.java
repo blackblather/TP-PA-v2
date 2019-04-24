e6f40e4a4702abedb33ca1f0cfbcb9bf679c7631
Joao-Surface <joao.monteiro95@hotmail.com>
2019-04-24 02:10:51 +0100

    Effect #3 (starting from #1, no #0), and effect #5 now olny increment the counter on the playerBoard. I had the wrong impression when I read the rules the 1st time

--- src/gamelogic/data/Upgrades.java
+++ src/gamelogic/data/Upgrades.java
@@ -9,20 +9,19 @@ class Upgrades {
     private ArrayList<Effect> upgrade = new ArrayList<>(Arrays.asList(
             new Effect(1, "Add one to Health", ()->gameDataHandler.GetPlayerBoard().HealHealthBy(1)),
             new Effect(1, "Repair one Hull", ()->gameDataHandler.GetShipBoard().HealHullBy(1)),
-            new Effect(2, "Build one Organic Detonator", (addInputs)->{
-                Room room = gameDataHandler.GetShipBoard().GetRooms().get(addInputs[0]);//Throws exception if it tries to get an invalid position
+            new Effect(2, "Build one Organic Detonator", ()->{
+                /*Room room = gameDataHandler.GetShipBoard().GetRooms().get(addInputs[0]);//Throws exception if it tries to get an invalid position
                 gameDataHandler.GetPlayerBoard().DecrementOrganicDetonatorCounter();    //Throws exception when trying to decrement below 0
-                room.IncrementOrganicDetonatorCounter();                                //Throws exception when trying to increment above Constants.MAX_ORGANIC_DETONATORS (never going to happen on this scenario)
-            }, "room"),
+                room.IncrementOrganicDetonatorCounter();*/
+                gameDataHandler.GetPlayerBoard().IncrementOrganicDetonatorCounter();
+            }),
             new Effect(4, "Add one to Movement", (addInputs)->{
                 CrewMember crewMember = gameDataHandler.GetPlayerBoard().GetCrewMembers().get(addInputs[0]);
                 crewMember.SetMovement(crewMember.GetMovement()+1);
             }, "crew member"),
-            new Effect(5, "Build one particle Desperser", (addInputs)->{
-                Room room = gameDataHandler.GetShipBoard().GetRooms().get(addInputs[0]);//Throws exception if it tries to get an invalid position
-                gameDataHandler.GetPlayerBoard().DecrementParticleDisperserCounter();   //Throws exception when trying to decrement below 0
-                room.IncrementParticleDisperserCounter();                               //Throws exception when trying to increment above Constants.MAX_ORGANIC_DETONATORS (never going to happen on this scenario)
-            }, "room"),
+            new Effect(5, "Build one particle Desperser", ()->{
+                gameDataHandler.GetPlayerBoard().IncrementParticleDisperserCounter();   //Throws exception when trying to decrement below 0
+            }),
             new Effect(5, "Gain one Sealed Room Token", ()->gameDataHandler.GetPlayerBoard().SetSealedRoomTokens(gameDataHandler.GetPlayerBoard().GetSealedRoomTokens()+1)),
             new Effect(6, "Gain one extra Attack Die", (addInputs)->{
                     CrewMember crewMember = gameDataHandler.GetPlayerBoard().GetCrewMembers().get(addInputs[0]);
