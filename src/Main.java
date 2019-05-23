5827929f81bdb9894ca8ba801e187d9ef275389d
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-05-23 22:47:59 +0100

    Main function now executes GUI instead of TextUI

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import gamelogic.Logic;
 import ui.text.TextUI;
+import ui.graphical.GraphicalUI;
 
 public class Main {
     public static void main(String[] args) {
-        TextUI textUI = new TextUI();
-        textUI.Start();
+        //TextUI textUI = new TextUI();
+        //textUI.Start();
+
+        GraphicalUI GUI = new GraphicalUI();
+        GUI.Run();
     }
 }
