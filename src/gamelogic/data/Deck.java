0d4526334b987afacad2cd55aa9a4428dd343048
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-05-24 21:53:34 +0100

    Added the rest of the crew member cards to the deck (still each crew member effects)

--- src/gamelogic/data/Deck.java
+++ src/gamelogic/data/Deck.java
@@ -5,8 +5,18 @@ import java.util.ArrayList;
 class Deck {
     private ArrayList<CrewMember> cards = new ArrayList<>(12);
     Deck(){
-        cards.add(new CrewMember(1,"Captain",1, 1, 3));
-        cards.add(new CrewMember(2,"Transporter Chief",0, 1));
+        cards.add(new CrewMember(1,"Doctor",1, 1));
+        cards.add(new CrewMember(2,"Comm's Officer",1, 1));
+        cards.add(new CrewMember(3,"Red Shirt",1, 1));
+        cards.add(new CrewMember(4,"Science Officer",1, 1));
+        cards.add(new CrewMember(5,"Engineer",1, 1));
+        cards.add(new CrewMember(6,"Captain",1, 1, 3));
+        cards.add(new CrewMember(7,"Commander",1, 1));
+        cards.add(new CrewMember(8,"Transporter Chief",0, 1));
+        cards.add(new CrewMember(9,"Moral Officer",1, 1));
+        cards.add(new CrewMember(10,"Security Officer",1, 2));
+        cards.add(new CrewMember(11,"Navigation Officer",2, 1));
+        cards.add(new CrewMember(12,"Shuttle Pilot",1, 1));
     }
     ArrayList<CrewMember> GetCards(){
         return cards;
