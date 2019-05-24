5bdcbf94e50f62e053e79424d3f13975968d5202
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-05-24 21:55:10 +0100

    Added logic.SelectCrewMembers() (Useful in GUI)

--- src/gamelogic/Logic.java
+++ src/gamelogic/Logic.java
@@ -6,6 +6,7 @@ import gamelogic.states.game.RoundPhase;
 import gamelogic.states.gameSetup.IGameSetupState;
 import gamelogic.states.gameSetup.InitialMenu;
 
+import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
 
@@ -31,28 +32,81 @@ public class Logic extends Observable {
     public GameDataHandler GetGameDataHandler(){ return gameDataHandler; }
 
     //Interact with gameSetupState:
-    public void InitialMenu(){ gameSetupState = new InitialMenu(gameDataHandler); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage()); }
-    public void NewGame(){ gameSetupState = gameSetupState.NewGame(); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
-    public void ExitGame() { gameSetupState = gameSetupState._Exit(); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
-    public void SelectDefaultJourney(){ gameSetupState = gameSetupState.SelectDefaultJourney(); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
-    public void SelectCustomJourney(String[] customJourneyStr){ gameSetupState = gameSetupState.SelectCustomJourney(customJourneyStr); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
-    public void SelectCrewMember(int deckPos){ gameSetupState = gameSetupState.SelectCrewMemberIn(deckPos); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
-    public void SetCrewMemberShipLocation(int roomPos, int crewMemberPos){ gameSetupState = gameSetupState._SetCrewMemberShipLocation(roomPos, crewMemberPos); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
-    public void EndGame(){ gameSetupState = gameSetupState._EndGame(); setChanged(); notifyObservers(gameDataHandler.GetErrorMessage());}
+    public void InitialMenu(){
+        gameSetupState = new InitialMenu(gameDataHandler);
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void NewGame(){
+        gameSetupState = gameSetupState.NewGame();
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void ExitGame() {
+        gameSetupState = gameSetupState._Exit();
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void SelectDefaultJourney(){
+        gameSetupState = gameSetupState.SelectDefaultJourney();
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void SelectCustomJourney(String[] customJourneyStr){
+        gameSetupState = gameSetupState.SelectCustomJourney(customJourneyStr);
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void SelectCrewMember(int deckPos){
+        gameSetupState = gameSetupState.SelectCrewMemberIn(deckPos);
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void SelectCrewMembers(ArrayList<Integer> deckPos){
+        gameSetupState = gameSetupState.SelectCrewMembers(deckPos);
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void SetCrewMemberShipLocation(int roomPos, int crewMemberPos){
+        gameSetupState = gameSetupState._SetCrewMemberShipLocation(roomPos, crewMemberPos);
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
+    public void EndGame(){
+        gameSetupState = gameSetupState._EndGame();
+        setChanged();
+        notifyObservers(gameDataHandler.GetErrorMessage());
+    }
 
     //Interact with gameState:
     public void StartGame(){
         gameDataHandler.LoadChosenCrewMemberSpecials();
         gameState = new RoundPhase(gameDataHandler);
     }
-    public void EvaluateRound(){ gameState = gameState.EvaluateRound(); }
-    public void Skip(){ gameState = gameState.Skip(); }
-    public void EvaluateChosenUpgrade(int opt){ gameState = gameState.EvaluateChosenUpgrade(opt-1); }
-    public void EvaluateChosenAction(int opt){ gameState = gameState.EvaluateChosenAction(opt-1); }
-    public void EvaluateChosenCrewMember(int opt){ gameState = gameState.EvaluateChosenCrewMember(opt-1); }
-    public void EvaluateChosenTrap(int opt){ gameState = gameState.EvaluateChosenTrap(opt-1); }
-    public void EvaluateChosenRoom(int opt){ gameState = gameState.EvaluateChosenRoom(opt-1); }
-    public void EvaluateAndExecuteEffect(){ gameState = gameState.EvaluateAndExecuteEffect(); }
+    public void EvaluateRound(){
+        gameState = gameState.EvaluateRound();
+    }
+    public void Skip(){
+        gameState = gameState.Skip();
+    }
+    public void EvaluateChosenUpgrade(int opt){
+        gameState = gameState.EvaluateChosenUpgrade(opt-1);
+    }
+    public void EvaluateChosenAction(int opt){
+        gameState = gameState.EvaluateChosenAction(opt-1);
+    }
+    public void EvaluateChosenCrewMember(int opt){
+        gameState = gameState.EvaluateChosenCrewMember(opt-1);
+    }
+    public void EvaluateChosenTrap(int opt){
+        gameState = gameState.EvaluateChosenTrap(opt-1);
+    }
+    public void EvaluateChosenRoom(int opt){
+        gameState = gameState.EvaluateChosenRoom(opt-1);
+    }
+    public void EvaluateAndExecuteEffect(){
+        gameState = gameState.EvaluateAndExecuteEffect();
+    }
 }
 
 
