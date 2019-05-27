64c74f784e252a9c744029113c76711489d0de57
Joao Monteiro <joao.monteiro95@hotmail.com>
2019-05-27 09:06:02 +0100

    Removed stupid "Enter"

--- src/gamelogic/data/Deck.java
+++ src/gamelogic/data/Deck.java
@@ -9,15 +9,13 @@ class Deck {
     private ArrayList<CrewMember> cards = new ArrayList<>(12);
     Deck(){
         cards.add(new CrewMember(1, Color.BLUE, "file:cards/Doctor.png", "Doctor",1, 1));
-        cards.add(new CrewMember(2, Color.BLACK, "file:cards/CommsOfficer.png", "Comm's Officer",1, 1));
+        cards.add(new CrewMember(2, Color.LIME, "file:cards/CommsOfficer.png", "Comm's Officer",1, 1));
         cards.add(new CrewMember(3, Color.ALICEBLUE, "file:cards/RedShirt.png", "Red Shirt",1, 1));
-        cards.add(new CrewMember(4, Color.ANTIQUEWHITE, "file:cards/ScienceOfficer.png", "Science Officer",1, 1));
+        cards.add(new CrewMember(4, Color.DARKSALMON, "file:cards/ScienceOfficer.png", "Science Officer",1, 1));
         cards.add(new CrewMember(5, Color.AQUAMARINE, "file:cards/Engineer.png", "Engineer",1, 1));
         cards.add(new CrewMember(6, Color.AZURE, "file:cards/Captain.png", "Captain",1, 1, 3));
         cards.add(new CrewMember(7, Color.CHARTREUSE, "file:cards/Commander.png", "Commander",1, 1));
-        cards.add(new CrewMember(8, Color.CRIMSON, "file:cards/Tra." +
-                "" +
-                "nsporterChief.png", "Transporter Chief",0, 1));
+        cards.add(new CrewMember(8, Color.CRIMSON, "file:cards/TransporterChief.png", "Transporter Chief",0, 1));
         cards.add(new CrewMember(9, Color.DARKMAGENTA, "file:cards/MoralOfficer.png", "Moral Officer",1, 1));
         cards.add(new CrewMember(10, Color.DEEPSKYBLUE, "file:cards/SecurityOfficer.png", "Security Officer",1, 2));
         cards.add(new CrewMember(11, Color.HOTPINK, "file:cards/NavigationOfficer.png", "Navigation Officer",2, 1));
